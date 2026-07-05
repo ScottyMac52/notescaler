570248ef70242dcb287f501e817730ef24920598
scottymac-cisco <300153936+scottymac-cisco@users.noreply.github.com>
2026-07-05 12:38:02 -0700

    Add string instrument factory service

---
+++ NoteScaler/Services/StringInstrumentFactory.cs
@@ -0,0 +1,15 @@
+namespace NoteScaler.Services
+{
+	using NoteScaler.Classes;
+	using NoteScaler.Enums;
+	using NoteScaler.Interfaces;
+	using NoteScaler.Services.Interfaces;
+
+	public sealed class StringInstrumentFactory : IStringInstrumentFactory
+	{
+		public IStringInstrument Create(TuningScheme tuningScheme, int numberOfFrets)
+		{
+			return new Guitar(tuningScheme, numberOfFrets);
+		}
+	}
+}
