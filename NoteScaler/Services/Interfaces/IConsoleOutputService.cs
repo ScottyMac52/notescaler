a37e07419faa8d9059d35ca4998a12fc710b815d
scottymac-cisco <300153936+scottymac-cisco@users.noreply.github.com>
2026-07-05 12:37:05 -0700

    Add console output service interface

---
+++ NoteScaler/Services/Interfaces/IConsoleOutputService.cs
@@ -0,0 +1,9 @@
+﻿namespace NoteScaler.Services.Interfaces
+{
+	using System;
+
+	public interface IConsoleOutputService
+	{
+		void WriteMessage(string message, ConsoleColor textColor = ConsoleColor.White);
+	}
+}
