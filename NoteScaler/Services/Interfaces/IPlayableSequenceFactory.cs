e421b6308395b1772772672f993958f3de1a3b96
scottymac-cisco <300153936+scottymac-cisco@users.noreply.github.com>
2026-07-05 12:37:23 -0700

    Add playable sequence factory interface

---
+++ NoteScaler/Services/Interfaces/IPlayableSequenceFactory.cs
@@ -0,0 +1,10 @@
+﻿namespace NoteScaler.Services.Interfaces
+{
+	using NoteScaler.Classes;
+	using NoteScaler.Config;
+
+	public interface IPlayableSequenceFactory
+	{
+		PlayableSequence Create(NoteScalerOptions options, int a4Reference);
+	}
+}
