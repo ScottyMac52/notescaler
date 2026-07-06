3ffb9588c082413865a721f8c28d560c5a03ebcd
scottymac-cisco <300153936+scottymac-cisco@users.noreply.github.com>
2026-07-05 21:34:40 -0700

    Fix music note cache interface namespace

--- NoteScaler/Services/Interfaces/IMusicNoteCache.cs
+++ NoteScaler/Services/Interfaces/IMusicNoteCache.cs
@@ -1,5 +1,7 @@
 namespace NoteScaler.Services.Interfaces
 {
+	using NoteScaler.Services;
+
 	public interface IMusicNoteCache
 	{
 		bool TryGet(string note, int a4Reference, out MusicNote musicNote);
