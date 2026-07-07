f30ab78451f0fe2e85012ea67de58cfdfec1b814
scottymac-cisco <300153936+scottymac-cisco@users.noreply.github.com>
2026-07-07 07:46:23 -0700

    Create string instruments from external definitions

--- NoteScaler/Services/StringInstrumentFactory.cs
+++ NoteScaler/Services/StringInstrumentFactory.cs
@@ -2,6 +2,7 @@ namespace NoteScaler.Services
 {
 	using NoteScaler.Enums;
 	using NoteScaler.Interfaces;
+	using NoteScaler.Models;
 	using NoteScaler.Services.Interfaces;
 
 	public sealed class StringInstrumentFactory : IStringInstrumentFactory
@@ -9,6 +10,11 @@ namespace NoteScaler.Services
 		public IStringInstrument Create(TuningScheme tuningScheme, int numberOfFrets)
 		{
 			return new Guitar(tuningScheme, numberOfFrets);
+		}
+
+		public IStringInstrument Create(StringInstrumentDefinition definition)
+		{
+			return new Guitar(definition);
 		}
 	}
 }
