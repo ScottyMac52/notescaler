f6f218f638119f7633b4a379ba7efae8b2e0af13
scottymac-cisco <300153936+scottymac-cisco@users.noreply.github.com>
2026-07-07 07:48:43 -0700

    Update unused string instrument factory test double

--- NoteScalerTests/Models/UnusedStringInstrumentFactory.cs
+++ NoteScalerTests/Models/UnusedStringInstrumentFactory.cs
@@ -2,12 +2,18 @@ namespace NoteScalerTests.Models
 {
 	using NoteScaler.Enums;
 	using NoteScaler.Interfaces;
+	using NoteScaler.Models;
 	using NoteScaler.Services.Interfaces;
 	using System;
 
 	public sealed class UnusedStringInstrumentFactory : IStringInstrumentFactory
 	{
 		public IStringInstrument Create(TuningScheme tuningScheme, int numberOfFrets)
+		{
+			throw new InvalidOperationException($"The string instrument factory should not be used by {nameof(UnusedStringInstrumentFactory)}.");
+		}
+
+		public IStringInstrument Create(StringInstrumentDefinition definition)
 		{
 			throw new InvalidOperationException($"The string instrument factory should not be used by {nameof(UnusedStringInstrumentFactory)}.");
 		}
