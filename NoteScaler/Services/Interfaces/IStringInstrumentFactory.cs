b7cf08b054a2fc5aeb335e26c648e1b7733e4cf0
scottymac-cisco <300153936+scottymac-cisco@users.noreply.github.com>
2026-07-07 07:45:39 -0700

    Support creating instruments from definitions

--- NoteScaler/Services/Interfaces/IStringInstrumentFactory.cs
+++ NoteScaler/Services/Interfaces/IStringInstrumentFactory.cs
@@ -2,9 +2,11 @@
 {
 	using NoteScaler.Enums;
 	using NoteScaler.Interfaces;
+	using NoteScaler.Models;
 
 	public interface IStringInstrumentFactory
 	{
 		IStringInstrument Create(TuningScheme tuningScheme, int numberOfFrets);
+		IStringInstrument Create(StringInstrumentDefinition definition);
 	}
 }
